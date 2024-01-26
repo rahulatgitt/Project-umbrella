09a39985629075c36f5a7bcfb1cdada8d476ed25
Rahul Kumar <134927946+rahulatgitt@users.noreply.github.com>
2024-01-26 22:15:53 +0530

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -3,21 +3,21 @@ import java.util.HashMap;
 public class hashmap {
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
-        map.put("devnagri", 22);
-        map.put("Rishikesh", 1);
-         map.put("Ram Janam Bhumi", 02);
-        map.put("Aayodhya", 03);
-         map.put("Bolenath ki nagri", 03);
-        map.put("Varanasi", 04);
+        map.put("dev", 22);
+        map.put("raj", 1);
+         map.put("hault", 02);
+        map.put("hex", 03);
+         map.put("text", 03);
+        map.put("keep", 04);
         
         System.out.println(map);
         System.out.println();
-        map.get("devnagri");
+        map.get("keep");
         int i = map.size();
-        map.replace("Aayodhya",33);
+        map.replace("rex",33);
         System.out.println(map);
         System.out.println("Size of map is "+ i);
-       boolean b = map.containsKey("Varanasi");
+       boolean b = map.containsKey("commit");
         System.out.println(b);
     }
 }
